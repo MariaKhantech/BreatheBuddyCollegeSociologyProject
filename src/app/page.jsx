6e2131bdf5e96abcd61b2f325dff52a85bec167c
MariaKhantech <mariatechfuture@gmail.com>
2026-05-05 21:10:41 -0400

    trying something else

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -105,7 +105,7 @@ export default function HomePage() {
         voices[0];
       utterance.voice = preferredVoice;
 
-      utterance.pitch = 1.5;
+      utterance.pitch = 1.7;
       utterance.rate = 1.2;
     }
 
