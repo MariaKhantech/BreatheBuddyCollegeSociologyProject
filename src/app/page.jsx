7fb9e51e03a216f1afddcdcbacb0d0ea9bdb9da7
MariaKhantech <mariatechfuture@gmail.com>
2026-05-05 21:14:33 -0400

    rate

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -106,7 +106,7 @@ export default function HomePage() {
       utterance.voice = preferredVoice;
 
       utterance.pitch = 1.7;
-      utterance.rate = 1.1;
+      utterance.rate = 1.0;
     }
 
     window.speechSynthesis.speak(utterance);
