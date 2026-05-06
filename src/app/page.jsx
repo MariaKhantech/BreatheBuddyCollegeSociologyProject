41799fcfd1056231ad494bc7d9e5367d58b87edd
MariaKhantech <mariatechfuture@gmail.com>
2026-05-05 21:05:38 -0400

    mobile voice

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -106,7 +106,7 @@ export default function HomePage() {
       utterance.voice = preferredVoice;
 
       utterance.pitch = 1.5;
-      utterance.rate = 1.2;
+      utterance.rate = 0.9;
     }
 
     window.speechSynthesis.speak(utterance);
