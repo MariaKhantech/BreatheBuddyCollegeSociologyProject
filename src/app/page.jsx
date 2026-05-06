eb9f9ccc463ac217a717a17c26e29cb4df48d45c
MariaKhantech <mariatechfuture@gmail.com>
2026-05-05 21:16:13 -0400

    fixing rate

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -106,7 +106,7 @@ export default function HomePage() {
       utterance.voice = preferredVoice;
 
       utterance.pitch = 1.7;
-      utterance.rate = 1.0;
+      utterance.rate = 0.9;
     }
 
     window.speechSynthesis.speak(utterance);
